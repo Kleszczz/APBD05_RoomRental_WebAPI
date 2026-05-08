039814e19503dc11d1afcfcd322ef47b58a4f33e
Kleszczz <mijoj.koworow@gmail.com>
2026-05-08 22:56:23 +0200

    Test data models

--- APBD05_RoomRental_WebAPI/Models/Reservation.cs
+++ APBD05_RoomRental_WebAPI/Models/Reservation.cs
@@ -1,6 +1,25 @@
-﻿namespace APBD05_RoomRental_WebAPI.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APBD05_RoomRental_WebAPI.Models;
 
 public class Reservation
 {
-    
+    public int Id { get; set; }
+
+    public int RoomId { get; set; }
+
+    [Required(ErrorMessage = "Organizer name is required.")]
+    public string OrganizerName { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Topic is required.")]
+    public string Topic { get; set; } = string.Empty;
+
+    public DateOnly Date { get; set; }
+
+    public TimeOnly StartTime { get; set; }
+
+    public TimeOnly EndTime { get; set; }
+
+    [Required]
+    public string Status { get; set; } = "planned";
 }
--- APBD05_RoomRental_WebAPI/Models/Room.cs
+++ APBD05_RoomRental_WebAPI/Models/Room.cs
@@ -1,6 +1,23 @@
-﻿namespace APBD05_RoomRental_WebAPI.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APBD05_RoomRental_WebAPI.Models;
 
 public class Room
 {
-    
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Room name is required.")]
+    public string Name { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Building code is required.")]
+    public string BuildingCode { get; set; } = string.Empty;
+
+    public int Floor { get; set; }
+
+    [Range(1, 500, ErrorMessage = "Room capacity must be between 1 and 500.")]
+    public int Capacity { get; set; }
+
+    public bool HasProjector { get; set; }
+
+    public bool IsActive { get; set; }
 }
