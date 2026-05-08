74eb25762b8a7fcd548f541265fb12c7c6ae58f9
Kleszczz <mijoj.koworow@gmail.com>
2026-05-09 00:06:45 +0200

    add rooms api endpoint and logic

--- APBD05_RoomRental_WebAPI/Controllers/RoomsController.cs
+++ APBD05_RoomRental_WebAPI/Controllers/RoomsController.cs
@@ -1,6 +1,105 @@
-﻿namespace APBD05_RoomRental_WebAPI.Controllers;
+﻿using APBD05_RoomRental_WebAPI.Models;
+using Microsoft.AspNetCore.Mvc;
 
-public class RoomsController
+namespace APBD05_RoomRental_WebAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class RoomsController : ControllerBase
 {
+    [HttpGet]
+    public IActionResult GetAllRooms(
+        [FromQuery] int? minCapacity,
+        [FromQuery] bool? hasProjector,
+        [FromQuery] bool? activeOnly
+    )
+    {
+        var rooms = Data.Rooms;
+
+        if (minCapacity.HasValue)
+        {
+            rooms = rooms.Where(r => r.Capacity > minCapacity.Value).ToList();
+        }
+
+        if (hasProjector.HasValue)
+        {
+            rooms = rooms.Where(r => r.HasProjector == hasProjector.Value).ToList();
+        }
+
+        if (activeOnly.HasValue)
+        {
+            rooms = rooms.Where(r => r.IsActive == activeOnly.Value).ToList();
+        }
+
+        return Ok(rooms);
+    }
+
+    [HttpGet("{id:int}")]
+    public IActionResult GetById(int id)
+    {
+        var room = Data.Rooms.FirstOrDefault(r => r.Id == id);
+
+        if (room == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(room);
+    }
+
+
+    [HttpGet("/building/{buildingCode}")]
+    public IActionResult GetRoomsFromBuilding(string buildingCode)
+    {
+        var rooms = Data.Rooms.Where(r => r.BuildingCode.Equals(buildingCode)).ToList();
+
+        return Ok(rooms);
+    }
+
+
+    [HttpPost]
+    public IActionResult Create([FromBody] Room room)
+    {
+        Data.Rooms.Add(room);
+        return CreatedAtAction(nameof(GetById), new { id = room.Id }, room);
+    }
+
+
+    [HttpPut("{id:int}")]
+    public IActionResult Update(int id, [FromBody] Room roomUpdates)
+    {
+        var room = Data.Rooms.FirstOrDefault(r => r.Id == id);
+
+        if (room == null)
+        {
+            return NotFound();
+        }
+
+        room.Name = roomUpdates.Name;
+        room.BuildingCode = roomUpdates.BuildingCode;
+        room.Floor = roomUpdates.Floor;
+        room.Capacity = roomUpdates.Capacity;
+        room.HasProjector = roomUpdates.HasProjector;
+        room.IsActive = roomUpdates.IsActive;
+
+        return Ok(room);
+    }
     
+    //DELETE /api/rooms/{id} Usuwa salę.
+    [HttpDelete("{id:int}")]
+    public IActionResult Delete(int id)
+    {
+        var room = Data.Rooms.FirstOrDefault(r => r.Id == id);
+
+        if (room == null)
+        {
+            return NotFound();
+        }
+        
+
+        //TODO: Tutaj trzeba dodac walidacje z wymagan projektu, cos z rezerwacjami i datami.
+
+        Data.Rooms.Remove(room);
+        return NoContent();
+    }
 }
